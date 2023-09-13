afcb7203d446869cf09097df0a9e5ad1fd7091cd
bcoderX <somearistide25@yahoo.com>
2023-09-13 13:38:32 +0000

    Disable image optimization

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'out',
+  images: {
+    unoptimized: true
 }
+}
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 
 module.exports = nextConfig
